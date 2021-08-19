cad78f1de41165de21b85906e515aa60a109f5bc
duruyao <duruyao@gmail.com>
2021-08-19 17:17:28 +0800

    refactor(server): refactor codes for initializing pkg

--- server/init.go
+++ server/init.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var UserHomeDir = func() string {
@@ -20,7 +22,43 @@ const (
 )
 
 func init() {
-	initConf()
-	initLog()
-	initDb()
+	initConfPath()
+	initLogDir()
+	initDbDir()
+}
+
+// initConfPath creates file '${HOME}/.GoChat/gochat.conf' if it doesn't exist.
+func initConfPath() {
+	// mkdir ${HOME}/.GoChat/
+	path := fmt.Sprintf(ConfFilePathFmt, UserHomeDir)
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	// echo ${confDefault} > ${HOME}/.GoChat/gochat.conf
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, _ = file.Write([]byte(confDefault))
+		defer func() { _ = file.Close() }()
+	}
+}
+
+// initDbDir creates directory '${HOME}/.GoChat/db' if it doesn't exist.
+func initDbDir() {
+	dir := fmt.Sprintf(DbFileDirFmt, UserHomeDir)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// initLogDir creates directory '${HOME}/.GoChat/log' if it doesn't exist.
+func initLogDir() {
+	dir := fmt.Sprintf(LogFileDirFmt, UserHomeDir)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
 }
