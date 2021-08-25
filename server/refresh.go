91e590965336fd3fcb42da495d537d0f8af95a08
duruyao <duruyao@gmail.com>
2021-08-25 16:03:02 +0800

    perf(server): improve server/refresh:10 GoRefreshLogFiles()

--- server/refresh.go
+++ server/refresh.go
@@ -3,31 +3,20 @@ package server
 import (
 	mlog "github.com/duruyao/gochat/server/log"
 	"log"
-	"sync"
 	"time"
 )
 
-var goRefreshLogFilesOnce sync.Once
-
 //
 func GoRefreshLogFiles() {
-	goRefreshLogFilesOnce.Do(func() {
+	refreshLogFiles()
+	duration := Tomorrow().Sub(time.Now())
+	select {
+	case <-time.After(duration):
 		refreshLogFiles()
-		duration := Tomorrow().Sub(time.Now())
-		select {
-		case <-time.After(duration):
-			refreshLogFiles()
-		}
-	loop:
-		for {
-			select {
-			case <-time.After(24 * time.Hour):
-				refreshLogFiles()
-			case <-Quit():
-				break loop
-			}
-		}
-	})
+	case <-Quit():
+		return
+	}
+	go GoRefreshLogFiles()
 }
 
 func refreshLogFiles() {
