5bc5d3392126c0abe7b144855879db9c0e17e755
duruyao <duruyao@gmail.com>
2021-08-25 12:53:06 +0800

    test(server): the TestInit() passed, and the TestRefreshLogFiles() untested

--- server_test/init_test.go
+++ server_test/init_test.go
@@ -1,15 +1,38 @@
-package server
+package server_test
 
 import (
-	sev "github.com/duruyao/gochat/server"
+	"github.com/duruyao/gochat/server"
+	mlog "github.com/duruyao/gochat/server/log"
 	"testing"
 	"time"
 )
 
 func TestInit(t *testing.T) {
-	defer sev.BeforeQuit.Do()
-	defer sev.WantQuit()
-	sev.Info("Load configuration:\n" + sev.MyConfig.String())
-	sev.Info("Load database:\n" + sev.MyRoomTable.String())
+	defer server.BeforeQuit.Do()
+	defer server.WantQuit()
+	mlog.InfoLn("Load configuration:\n" + server.MyConfig.String())
+	mlog.InfoLn("Load database:\n" + server.MyRoomTable.String())
 	time.Sleep(3 * time.Second)
+}
+
+func TestRefreshLogFiles(t *testing.T) {
+	defer func() {
+		server.BeforeQuit.Do()
+	}()
+	go func() {
+		select {
+		case <-time.After(1 * time.Hour):
+			server.WantQuit()
+		}
+	}()
+loop:
+	for {
+		select {
+		case <-time.After(time.Minute):
+			timeStr := time.Now().Format("2006-01-02 03:04:05")
+			mlog.DebugLn("Current time: " + timeStr)
+		case <-server.Quit():
+			break loop
+		}
+	}
 }
