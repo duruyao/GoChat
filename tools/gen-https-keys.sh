6c50764dd263e65a4817148049993e752f159bc0
duruyao <duruyao@gmail.com>
2021-09-03 19:14:17 +0800

    build(tools): remove tools/

--- tools/gen-https-keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-key_path="${HOME}/.GoChat/.key/.server.key"
-crt_path="${HOME}/.GoChat/.key/.server.crt"
-input="CN\nZhejiang\nHangzhou\n.\n.\n.\n.\n"
-
-openssl rand -writerand "${HOME}/.rnd" && \
-openssl genrsa -out "${key_path}" 2048 && \
-printf "${input}" | xargs `openssl req -new -x509 -sha256 -key "${key_path}" -out "${crt_path}" -days 3650`
