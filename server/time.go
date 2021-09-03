a212e4e33332237d4214e98b0c7956df4668610f
duruyao <duruyao@gmail.com>
2021-09-03 19:11:04 +0800

    refactor(server): remove old files

--- server/time.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "time"
-
-// return Tomorrow 00:00:00.000.
-func Tomorrow() time.Time {
-	t := time.Now().AddDate(0, 0, 1)
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-}
