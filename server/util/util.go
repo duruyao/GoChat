19f916a373aaf15b1dc85bc93454f1f6d0dfbb3e
duruyao <duruyao@gmail.com>
2021-09-03 18:45:25 +0800

    feat(util): add CreateUUId() and Encrypt()

--- server/util/util.go
+++ server/util/util.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"crypto/rand"
+	"crypto/sha512"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,7 +14,7 @@ import (
 
 // Tomorrow() returns an object of type time.Time whose value is 00:00:00.000 of the next day.
 func Tomorrow() time.Time {
-	t := time.Now().AddDate(0, 0, 1)
+	t := time.Now().Local().AddDate(0, 0, 1)
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
@@ -75,3 +78,23 @@ var quitOnce sync.Once
 
 //
 func SetQuit() { quitOnce.Do(func() { close(quit) }) }
+
+// CreateUUID creates a random UUID with from RFC 4122
+// adapted from http://github.com/nu7hatch/gouuid
+func CreateUUID() string {
+	u := new([16]byte)
+	_, err := rand.Read(u[:])
+	if err != nil {
+		log.Fatalln("Cannot generate UUID", err)
+	}
+	// 0x40 is reserved variant from RFC 4122
+	u[8] = (u[8] | 0x40) & 0x7F
+	// Set the four most significant bits (bits 12 through 15) of the
+	// time_hi_and_version field to the 4-bit version number.
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+	return fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+}
+
+func Encrypt(plaintext string) string {
+	return fmt.Sprintf("%x", sha512.Sum512([]byte(plaintext)))
+}
