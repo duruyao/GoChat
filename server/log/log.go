45e68cf6d16ee12677806c01884de924304101a7
duruyao <duruyao@gmail.com>
2021-09-09 10:03:21 +0800

    feat(log): export ErrorLogger, FatalLogger ... from the server/log package

--- server/log/log.go
+++ server/log/log.go
@@ -11,65 +11,65 @@ import (
 const flags = log.Ltime | log.Llongfile
 
 var (
-	debugLogger *log.Logger
-	infoLogger  *log.Logger
-	errorLogger *log.Logger
-	panicLogger *log.Logger
-	fatalLogger *log.Logger
+	DebugLogger *log.Logger
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+	PanicLogger *log.Logger
+	FatalLogger *log.Logger
 )
 
 var refreshLoggerOnce sync.Once
 
 //
 func refreshLogger() {
-	debugLogger = log.New(io.MultiWriter(os.Stdout, files["all"], files["debug"]), "[debug] ", flags)
-	infoLogger = log.New(io.MultiWriter(os.Stdout, files["all"], files["info"]), "[info ] ", flags)
-	errorLogger = log.New(io.MultiWriter(os.Stderr, files["all"], files["error"]), "[error] ", flags)
-	panicLogger = log.New(io.MultiWriter(os.Stderr, files["all"], files["panic"]), "[panic] ", flags)
-	fatalLogger = log.New(io.MultiWriter(os.Stderr, files["all"], files["fatal"]), "[fatal] ", flags)
+	DebugLogger = log.New(io.MultiWriter(os.Stdout, files["all"], files["debug"]), "[debug] ", flags)
+	InfoLogger = log.New(io.MultiWriter(os.Stdout, files["all"], files["info"]), "[info ] ", flags)
+	ErrorLogger = log.New(io.MultiWriter(os.Stderr, files["all"], files["error"]), "[error] ", flags)
+	PanicLogger = log.New(io.MultiWriter(os.Stderr, files["all"], files["panic"]), "[panic] ", flags)
+	FatalLogger = log.New(io.MultiWriter(os.Stderr, files["all"], files["fatal"]), "[fatal] ", flags)
 }
 
 //
 func Debug(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = debugLogger.Output(2, fmt.Sprint(v...))
+	_ = DebugLogger.Output(2, fmt.Sprint(v...))
 }
 
 //
 func DebugF(format string, v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = debugLogger.Output(2, fmt.Sprintf(format, v...))
+	_ = DebugLogger.Output(2, fmt.Sprintf(format, v...))
 }
 
 //
 func DebugLn(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = debugLogger.Output(2, fmt.Sprintln(v...))
+	_ = DebugLogger.Output(2, fmt.Sprintln(v...))
 }
 
 //
 func Info(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = infoLogger.Output(2, fmt.Sprint(v...))
+	_ = InfoLogger.Output(2, fmt.Sprint(v...))
 }
 
 //
 func InfoF(format string, v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = infoLogger.Output(2, fmt.Sprintf(format, v...))
+	_ = InfoLogger.Output(2, fmt.Sprintf(format, v...))
 }
 
 //
 func InfoLn(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = infoLogger.Output(2, fmt.Sprintln(v...))
+	_ = InfoLogger.Output(2, fmt.Sprintln(v...))
 }
 
 //
 func Panic(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
 	s := fmt.Sprint(v...)
-	_ = panicLogger.Output(2, s)
+	_ = PanicLogger.Output(2, s)
 	panic(s)
 }
 
@@ -77,7 +77,7 @@ func Panic(v ...interface{}) {
 func PanicF(format string, v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
 	s := fmt.Sprintf(format, v...)
-	_ = panicLogger.Output(2, s)
+	_ = PanicLogger.Output(2, s)
 	panic(s)
 }
 
@@ -85,42 +85,42 @@ func PanicF(format string, v ...interface{}) {
 func PanicLn(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
 	s := fmt.Sprintln(v...)
-	_ = panicLogger.Output(2, s)
+	_ = PanicLogger.Output(2, s)
 	panic(s)
 }
 
 //
 func Error(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = errorLogger.Output(2, fmt.Sprint(v...))
+	_ = ErrorLogger.Output(2, fmt.Sprint(v...))
 }
 
 //
 func ErrorF(format string, v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = errorLogger.Output(2, fmt.Sprintf(format, v...))
+	_ = ErrorLogger.Output(2, fmt.Sprintf(format, v...))
 }
 
 //
 func ErrorLn(v ...interface{}) {
 	refreshLoggerOnce.Do(refreshLogger)
-	_ = errorLogger.Output(2, fmt.Sprintln(v...))
+	_ = ErrorLogger.Output(2, fmt.Sprintln(v...))
 }
 
 //
 func Fatal(v ...interface{}) {
-	_ = fatalLogger.Output(2, fmt.Sprint(v...))
+	_ = FatalLogger.Output(2, fmt.Sprint(v...))
 	os.Exit(1)
 }
 
 //
 func FatalF(format string, v ...interface{}) {
-	_ = fatalLogger.Output(2, fmt.Sprintf(format, v...))
+	_ = FatalLogger.Output(2, fmt.Sprintf(format, v...))
 	os.Exit(1)
 }
 
 //
 func FatalLn(v ...interface{}) {
-	_ = fatalLogger.Output(2, fmt.Sprintln(v...))
+	_ = FatalLogger.Output(2, fmt.Sprintln(v...))
 	os.Exit(1)
 }
