c33b92128556f537874bb018e49f7069853dad7c
duruyao <duruyao@gmail.com>
2021-09-11 14:22:41 +0800

    style(main): add some codes (untested)

--- server/main/main.go
+++ server/main/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/duruyao/gochat/server"
+import (
+	"github.com/duruyao/gochat/server/backend"
+	"github.com/duruyao/gochat/server/util"
+	"github.com/duruyao/gochat/server/web"
+)
 
 func main() {
-	server.Run()
+	go web.GoRunWebApp()
+	go backend.GoRunWebServer(":8181")
+	go backend.GoRunChatServer(":8282")
+
+	select {
+	case <-util.Quit():
+		return
+	}
 }
